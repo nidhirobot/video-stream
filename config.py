e1fa52f17c02837218c4dbb09f8bf8d0c4a24095
Aju <73631177+nidhirobot@users.noreply.github.com>
2022-02-06 21:38:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,23 +4,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-MONGODB_URL = getenv("MONGODB_URL")
-OWNER_NAME = getenv("OWNER_NAME")
-ALIVE_NAME = getenv("ALIVE_NAME")
-BOT_USERNAME = getenv("BOT_USERNAME")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+SESSION_NAME = "AQCs9rnUjJZ6r72_bkFkZMQReNq6mOQr7eaLNG_ZKDRtc1TL_b82tJTLIdDR9IrWIiIC3iirXCm0gLHsM_obmzDyqMoWrzqpUhO2-iQQ6MWJnOU7t4dBv67zSt7xFSZe15LA7V4k2n0RdUcGDkAom7m0A3tUHr6p0u0SVGocA_pMqME2oR3YUOcxSaJuayXFbgg74KgzVx6l9QEUSSCg8LuRqUUItlUFFterFWDSNMr3byQtUx5VbLk7jFWBC8yoUADFtBbd2-qGMWpDxwn0NUtDXGykeKour7iJ6BErQnc6JqRIB0zTuTvOmCyQSY3988_8X5qLb1T3D0N2N_6iB8lrAAAAAS4eqkIA"
+BOT_TOKEN = "1825723267:AAH2qVgWlGu0Xj4i0kdZlSJ73TFyGf_akgs"
+BOT_NAME = "Couple bot"
+API_ID = "14463553"
+API_HASH = "4b85dd6a081e4e2e0c8e7f1244614327"
+MONGODB_URL = "mongodb+srv://Kaju:Db@kajudb.hkebg.mongodb.net/KajuDb?retryWrites=true&w=majority"
+OWNER_NAME = "AJU"
+ALIVE_NAME = "AJU"
+BOT_USERNAME = "Couplerobot"
+ASSISTANT_NAME = "Music player"
+GROUP_SUPPORT = "Indianbinner_chats"
+UPDATES_CHANNEL = "Indianbinner_chats"
+SUDO_USERS = "1237452850"
+COMMAND_PREFIXES = "/"
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO")
+DURATION_LIMIT = "60"
+UPSTREAM_REPO = "https://github.com/nidhirobot/video-stream"
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
 IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
